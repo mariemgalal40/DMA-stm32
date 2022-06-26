0384af710ffc82bbd388b45273a6de562f0d167c
mariem galal <61452443+mariemgalal40@users.noreply.github.com>
2022-06-26 01:56:26 -0700

    dma

---
+++ DMA/Debug/Core/Src/DMA.d
@@ -0,0 +1,8 @@
+Core/Src/DMA.o: ../Core/Src/DMA.c ../Core/Inc/DMA.h ../Core/Inc/GPIO.h \
+ ../Core/Inc/EXIT.h
+
+../Core/Inc/DMA.h:
+
+../Core/Inc/GPIO.h:
+
+../Core/Inc/EXIT.h:
---
+++ DMA/Debug/Core/Src/main.d
@@ -0,0 +1,8 @@
+Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/GPIO.h \
+ ../Core/Inc/USART.h ../Core/Inc/DMA.h
+
+../Core/Inc/GPIO.h:
+
+../Core/Inc/USART.h:
+
+../Core/Inc/DMA.h:
